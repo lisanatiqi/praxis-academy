aa8666bdbe41bb30b0632476a407dc90692ba637
lisanatiqi <56305974+lisanatiqi@users.noreply.github.com>
2019-10-23 11:00:55 +0700

    Add files via upload

---
+++ novice/01-01/kasus/matrikmultidimensi.java
@@ -0,0 +1,25 @@
+class mmatrikmultidimensi {
+
+    public static void main(String[] args) {
+        int[][] cinema = new int[5][5];
+        
+        cinema[2][2] = 1;
+        for (int i = 1; i < 4; i++)
+        {
+            cinema[i][3] = 1;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            cinema[i][4] = 1;
+        }
+
+        for (int j = 0; j < cinema[0].length; j++)
+        {
+            for (int i = 0; i < cinema.length; i++)
+            {
+                System.out.print(cinema[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
