d98541be477a502be888ee0b096576a6961dbb0a
lisanatiqi <56305974+lisanatiqi@users.noreply.github.com>
2019-10-31 16:19:48 +0700

    Add files via upload

---
+++ novice/02-04/src/main/java/Dependency/Injection/MyLegacyTest.java
@@ -0,0 +1,7 @@
+public class MyLegacyTest {
+
+    public static void main(String[] args) {
+        MyApplication app = new MyApplication();
+        app.processMessages("Hi Paijo", "paijo@abc.com");
+    }
+}
